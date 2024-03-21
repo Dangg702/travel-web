883909adc526d7981b2d46d64da78c9c09d84cd6
TRANHOANGTUAN <90249402+jackeyiny@users.noreply.github.com>
2024-03-21 12:43:13 +0700

    fix conflicts

--- src/models/Review.js
+++ src/models/Review.js
@@ -4,9 +4,7 @@ const Schema = mongoose.Schema;
 const reviewSchema = new Schema(
     {
         userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-        userName: { type: mongoose.Schema.Types.String, ref: 'User', required: true },
         tourId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tour', required: true },
-        tourName: { type: mongoose.Schema.Types.String, ref: 'Tour', required: true },
         rating: { type: Number },
         comment: { type: String },
     },
