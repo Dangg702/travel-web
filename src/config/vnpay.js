4a5c3bf9a7357c251f2b146876a905f180313ea1
TRANHOANGTUAN <90249402+jackeyiny@users.noreply.github.com>
2024-04-04 15:48:51 +0700

    hoho

--- src/config/vnpay.js
+++ src/config/vnpay.js
@@ -3,8 +3,7 @@ module.exports = {
         vnp_TmnCode: 'B9J82O5K',
         vnp_HashSecret: 'CYKEBWCVDUWVRNICDHGUWDGRNNVXZGBS',
         vnp_Url: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
-        vnp_ReturnUrl: 'http://localhost:9000/order/vnpay_return',
-        vnp_Api:"https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
-        vnp_ReturnUrl: "http://localhost:8888/order/vnpay_return"
+        vnp_ReturnUrl: 'http://localhost:9000/api/payment/vnpay_return',
+        vnp_Api:"https://sandbox.vnpayment.vn/merchant_webapi/api/transaction"
     },
 };
