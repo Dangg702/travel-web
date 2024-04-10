38924c5d9ad6f42e58ce37f27b48d32c31d3ad94
TRANHOANGTUAN <90249402+jackeyiny@users.noreply.github.com>
2024-04-10 17:10:45 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ dotenvFlow.config();
 // dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5000;
 
 const route = require('./routes');
 const db = require('./config/db');
