8c976d09892f7320b2f5659446adfe343a7f3b81
Hai Dang Tran <95269870+Dangg702@users.noreply.github.com>
2024-08-06 20:01:56 +0700

    Update vnpay.js

--- src/config/vnpay.js
+++ src/config/vnpay.js
@@ -3,10 +3,10 @@ dotenvFlow.config();
 
 module.exports = {
     vnpay: {
-        vnp_TmnCode: 'B9J82O5K',
-        vnp_HashSecret: 'CYKEBWCVDUWVRNICDHGUWDGRNNVXZGBS',
+        vnp_TmnCode: 'OHR5LZ0M',
+        vnp_HashSecret: 'L3LHBTVZC3JY4XX3VP71DJG4DHWQ703B',
         vnp_Url: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
-        vnp_ReturnUrl: 'https://travel-web-ks0z.onrender.com/api/payment/vnpay_return',
+        vnp_ReturnUrl: 'https://travel-web-lyc3.onrender.com/api/payment/vnpay_return',
         vnp_Api: 'https://sandbox.vnpayment.vn/merchant_webapi/api/transaction',
     },
 };
